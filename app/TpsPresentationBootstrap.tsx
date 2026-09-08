c2dd67c83b1d5b03736e05400995d26f8b54df0e
yz4git <65384484+yz4git@users.noreply.github.com>
2026-09-09 01:08:22 +0900

    Install TPS throw staging presentation

--- app/TpsPresentationBootstrap.tsx
+++ app/TpsPresentationBootstrap.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { installTpsGuardClashPresentation } from "@/src/game/tps-guard-clash";
+import { installTpsThrowStagingPresentation } from "@/src/game/tps-throw-staging";
 
 installTpsGuardClashPresentation();
+installTpsThrowStagingPresentation();
 
 export default function TpsPresentationBootstrap() {
   return null;

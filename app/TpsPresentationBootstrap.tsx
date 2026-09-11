82b5a95548574cde6ca5a04a2765dc85152f4a87
yz4git <65384484+yz4git@users.noreply.github.com>
2026-09-11 11:48:15 +0900

    Remove redundant rapid-kick bridge layer

--- app/TpsPresentationBootstrap.tsx
+++ app/TpsPresentationBootstrap.tsx
@@ -19,7 +19,6 @@ import { installTpsImpactLensPulsePresentation } from "@/src/game/tps-impact-len
 import { installTpsInterceptSilhouettePresentation } from "@/src/game/tps-intercept-silhouette";
 import { installTpsKickContactShapingPresentation } from "@/src/game/tps-kick-contact-shaping";
 import { installTpsKickPairSpacingPresentation } from "@/src/game/tps-kick-pair-spacing";
-import { installTpsKickSpamGroundBridgePresentation } from "@/src/game/tps-kick-spam-ground-bridge";
 import { installTpsKickSupportFootPresentation } from "@/src/game/tps-kick-support-foot";
 import { installTpsLatestImpactWavePresentation } from "@/src/game/tps-latest-impact-wave";
 import { installTpsLowKickOpenLinePresentation } from "@/src/game/tps-lowkick-open-line";
@@ -56,7 +55,6 @@ installTpsKickContactShapingPresentation();
 installTpsKickSupportFootPresentation();
 installTpsDashKickGroundContactPresentation();
 installTpsKickPairSpacingPresentation();
-installTpsKickSpamGroundBridgePresentation();
 
 export default function TpsPresentationBootstrap() {
   return null;
